e4ec63d53ae278f3675280a6dd3a4d0edd705522
haifei.zhang <haifei.zhang@chaitin.com>
2018-07-29 08:22:54 +0800

    sturct add bson tag

--- modelm/models.go
+++ modelm/models.go
@@ -3,17 +3,17 @@ package model
 import time "time"
 
 type Mood struct {
-	ID      string    `json:"id"`
-	User    string    `json:"user"`
-	Score   int       `json:"score"`
-	Comment *string   `json:"comment"`
-	Time    time.Time `json:"time"`
+	ID      string    `json:"id" bson:"_id,omitempty"`
+	User    string    `json:"user" bson:"user"`
+	Score   int       `json:"score" bson:"score"`
+	Comment *string   `json:"comment" bson:"comment"`
+	Time    time.Time `json:"time" bson:"time"`
 }
 
 type User struct {
-	ID       string   `json:"id"`
-	Sex      Sex      `json:"sex"`
-	Username string   `json:"username"`
-	Password string   `json:"password"`
-	Moods    []string `json:"moods"`
+	ID       string   `json:"id_" bson:"_id"`
+	Sex      Sex      `json:"sex" bson:"sex"`
+	Username string   `json:"username" bson:"username"`
+	Password string   `json:"password" bson:"password"`
+	Moods    []string `json:"moods" bson:"moods"`
 }
